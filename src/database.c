d50ad0ebce3bfe8bed738e2e0b520fefaba2e0b5
neurmancer <m.arda.safak06@gmail.com>
2026-09-08 20:08:19 +0300

    Fuck...I am lexer guy now?

--- src/database.c
+++ src/database.c
@@ -1,6 +1,3 @@
-#define BUFFER_SIZE 512
-#define TODO_RELATIVE_DB "/db/todo.db"
-
 #include "database.h"
 #include <sqlite3.h>
 #include <stdio.h>
@@ -16,17 +13,23 @@
 #define BUFFER_SIZE 512
 #define TODO_RELATIVE_DB "/db/todo.db"
 
-char *sql_queries_or_whatever[] = {
-    "insert into TODOS(Title,Completed) values(?,?)",
 
-
+const char *sql_queries_or_whatever[] = {
+    "INSERT INTO TODOS(Title) VALUES(?)",
+    "DELETE FROM TODOS WHERE ID=?",
+    "UPDATE TODOS SET COMPLETED=? WHERE ID=?"
 };
 
-enum SQL_THINGIES { NEW_TODO , DEL_TODO, UPDATE_TODO};
-enum SQL_TAGS {TITLE = 1, COMPLETED = 2};
+
+enum SQL_THINGIES { 
+    NEW_TODO = 0, 
+    DEL_TODO = 1, 
+    UPDATE_TODO = 2 
+};
 
 sqlite3 *set_db(void)
 {
+    //Note to future-self try goto cleanup pattern next time
     sqlite3 *database = NULL;
     char env_buffer[BUFFER_SIZE] = { 0 };
     char db_path[BUFFER_SIZE] = { 0 };
@@ -43,10 +46,10 @@ sqlite3 *set_db(void)
         fprintf(stderr, "Error: Database path exceeds BUFFER_SIZE.\n");
         return(NULL);
     }
+    
     int rc = sqlite3_open(db_path, &database);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open the database: %s\n", sqlite3_errmsg(database));
-        
         if (database != NULL) {
             sqlite3_close(database);
         }
@@ -56,33 +59,27 @@ sqlite3 *set_db(void)
     return(database);   
 }
 
-
 enum STATUS add_todo(sqlite3 *db, struct todo_data *data)
 {
-
     sqlite3_stmt *res = NULL;
 
     int rc = sqlite3_prepare_v2(db, sql_queries_or_whatever[NEW_TODO], -1, &res, 0);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "To-Do couldn't added:%s\n", sqlite3_errmsg(db));
+        fprintf(stderr, "To-Do prepare error: %s\n", sqlite3_errmsg(db));
         return(U_FUCKED);
     }
 
-    if(sqlite3_bind_text(res, TITLE,data->title,-1, SQLITE_TRANSIENT) != SQLITE_OK)
+    if(sqlite3_bind_text(res, 1, data->title, -1, SQLITE_TRANSIENT) != SQLITE_OK)
     {
-        fprintf(stderr, "first bind err: %s\n", sqlite3_errmsg(db));
+        fprintf(stderr, "First bind err: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(res); 
         return(U_FUCKED);
     }
 
-    if(sqlite3_bind_int(res, COMPLETED,data->is_done) != SQLITE_OK)
-    {
-        fprintf(stderr, "FUCKKKK Second bind err: %s\n", sqlite3_errmsg(db));
-        return(U_FUCKED);
-    }
-    
     rc = sqlite3_step(res);
     if (rc != SQLITE_DONE) {
-        fprintf(stderr, "Something happeend it is:%s\n", sqlite3_errmsg(db));
+        fprintf(stderr, "Step error: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(res);
         return(U_FUCKED);
     }
 
@@ -92,15 +89,88 @@ enum STATUS add_todo(sqlite3 *db, struct todo_data *data)
 
 enum STATUS delete_todo(sqlite3 *db, struct todo_data *data)
 {
+    sqlite3_stmt *res = NULL;
 
+    int rc = sqlite3_prepare_v2(db, sql_queries_or_whatever[DEL_TODO], -1, &res, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Delete prepare error: %s\n", sqlite3_errmsg(db));
+        return(U_FUCKED);
+    }
 
+    if (sqlite3_bind_int(res, 1, data->id) != SQLITE_OK) {
+        fprintf(stderr, "Delete bind err: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(res);
+        return(U_FUCKED);
+    }
+
+    rc = sqlite3_step(res);
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Delete step error: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(res);
+        return(U_FUCKED);
+    }
+
+    sqlite3_finalize(res);
     return(OK);
 }
 
 enum STATUS update_todo(sqlite3 *db, struct todo_data *data)
 {
+    sqlite3_stmt *res = NULL;
 
+    int rc = sqlite3_prepare_v2(db, sql_queries_or_whatever[UPDATE_TODO], -1, &res, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Update prepare error: %s\n", sqlite3_errmsg(db));
+        return(U_FUCKED);
+    }
 
+    
+    if (sqlite3_bind_int(res, 1, data->is_done) != SQLITE_OK) {
+        fprintf(stderr, "Update completed bind err: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(res);
+        return(U_FUCKED);
+    }
+
+    if (sqlite3_bind_int(res, 2, data->id) != SQLITE_OK) {
+        fprintf(stderr, "Update ID bind err: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(res);
+        return(U_FUCKED);
+    }
+
+    rc = sqlite3_step(res);
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Update step error: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(res);
+        return(U_FUCKED);
+    }
+
+    sqlite3_finalize(res);
     return(OK);
 }
 
+//Demo CLI version before implementing this shit to web
+enum STATUS print_all_todo_titles(sqlite3 *db)
+{
+    sqlite3_stmt *res = NULL;
+    
+    const char *sql = "SELECT Title FROM TODOS;";
+
+    int rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return(U_FUCKED);
+    }
+
+    printf("--- TODO TITLES ---\n");
+    
+    while (sqlite3_step(res) == SQLITE_ROW) {
+        const unsigned char *title = sqlite3_column_text(res, 0);
+        
+        if (title != NULL) {
+            printf("- %s\n", title);
+        }
+    }
+
+    sqlite3_finalize(res);
+    return(OK);
+}
--- src/database.h
+++ src/database.h
@@ -3,14 +3,13 @@
 
 #include <sqlite3.h>
 #include "utils.h"
-//Why doing this? 'cuz I don't want some prick with '; DROP TABLE 'todos'; trick fuck up my todo app with a fucking injection
 
 
-//note to future-self change tha names
 
 sqlite3 *set_db(void);
 
 //handling shit
+enum STATUS print_all_todo_titles(sqlite3 *db);
 enum STATUS add_todo(sqlite3 *db, struct todo_data *data);
 enum STATUS delete_todo(sqlite3 *db, struct todo_data *data);
 enum STATUS update_todo(sqlite3 *db, struct todo_data *data);
